9f2a4ac3f72c770a8696746c55e434fc826ff585
minnja6 <jasmine.minniweather@verizon.com>
2019-09-11 17:58:22 -0500

    initial installs
    No code of my own added yet, I need to test next and make sure that the api and client connect to each other

--- api/app.js
+++ api/app.js
@@ -5,13 +5,13 @@ const express = require('express');
 const path = require('path');
 const indexRouter = require('./routes/courses');
 const usersRouter = require('./routes/users');
-
+var cors = require('cors');
 // create the Express app
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/api', indexRouter);
 app.use('/api', usersRouter);
 
